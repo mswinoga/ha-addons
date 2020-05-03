e8613a4066c3e1a74c88b38c17e65ba4105e0730
Marcin Swinoga <marcin.swinoga@gmail.com>
2020-05-03 21:25:01 +0200

    update

--- modbus-mqtt/run.sh
+++ modbus-mqtt/run.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/with-contenv bashio
 
-CONFIG_PATH=/data/options.json
-
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
-
-python3 /gateway/runner.py
-
+CONFIG_PATH=/data/options.json; MQTT_HOST=$(bashio::services mqtt "host"); MQTT_USER=$(bashio::services mqtt "username"); MQTT_PASSWORD=$(bashio::services mqtt "password"); python3 /gateway/runner.py 
