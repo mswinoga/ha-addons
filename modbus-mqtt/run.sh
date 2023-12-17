96e5e73d483328078851e00025c47d6c1fa1a545
Marcin Swinoga <marcin.swinoga@gmail.com>
2023-12-17 21:40:40 +0100

    wip

--- modbus-mqtt/run.sh
+++ modbus-mqtt/run.sh
@@ -5,4 +5,5 @@ export MQTT_HOST=$(bashio::services mqtt "host")
 export MQTT_USER=$(bashio::services mqtt "username")
 export MQTT_PASSWORD=$(bashio::services mqtt "password")
 
+source /data/bin/activate
 python3 /gateway/runner.py 
